6743501a1f0ca8eef64e2a226d844beb1ab552f8
tTargiel <13293432+tTargiel@users.noreply.github.com>
2023-08-20 16:13:09 +0200

    Implemented full functionality of reading operation (subpixel layout, pattern, order)

--- logic.py
+++ logic.py
@@ -1,7 +1,9 @@
+import re
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QFileDialog, QGraphicsScene
+from PyQt6.QtWidgets import QFileDialog, QGraphicsScene, QMessageBox
 from PyQt6.QtGui import QPixmap
 from PIL import Image
+from urllib.parse import unquote
 
 class logic():
     def writeOutput(window, text):
@@ -15,35 +17,123 @@ class logic():
             if selected_files:
                 selected_file = selected_files[0]
                 window.ui.lineEdit_path.setText(selected_file) # Update the lineEdit_path with the selected file
-                logic.displayImage(window, selected_file) # Load and display the selected image
+                try:
+                    logic.displayImage(window, selected_file) # Load and display the selected image
+                except:
+                    logic.writeOutput(window, "Something went wrong.")
 
     def displayImage(window, path):
-        if path.startswith("file://"):
-            path = path[7:].strip() # Remove "file://" prefix and any spaces
-        window.ui.lineEdit_path.setText(path)
-        pixmap = QPixmap(path)
-        scene = QGraphicsScene()
-        scene.addPixmap(pixmap)
-        window.ui.graphicsView_image.setScene(scene)
-        window.ui.graphicsView_image.fitInView(scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
+        path = unquote(path.strip())
+        if logic.validate_path(window, path):  # Check if the path is valid
+            if path.startswith("file://"):
+                path = path[7:]  # Remove "file://" prefix
+            window.ui.lineEdit_path.setText(path)
+            pixmap = QPixmap(path)
+            scene = QGraphicsScene()
+            scene.addPixmap(pixmap)
+            window.ui.graphicsView_image.setScene(scene)
+            window.ui.graphicsView_image.fitInView(scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
+            logic.writeOutput(window, "")
+        elif path == "":
+            logic.writeOutput(window, "")
+        else:
+            # QMessageBox.warning(window, "Invalid File Path", "The file path is not valid.")
+            logic.writeOutput(window, "The file path is not valid. Are you sure, you already specified image file?")
 
-    def readOnly(window, option):
+    def validate_path(self, path):
+        regex_pattern = r"^(.+)\/([^\/]+)\.(ppm|png|jpg|jpeg|gif|tiff|bmp)$"
+        return re.match(regex_pattern, path, re.IGNORECASE)
+
+    def readOperation(window, option):
+        window.ui.textEdit_text.setReadOnly(option)
+        window.ui.label_number.setVisible(option)
+        window.ui.spinBox_number.setVisible(option)
+
+    def showNumber(window, option):
         window.ui.textEdit_text.setReadOnly(option)
 
-    def readHidden(window, path):
-        # Testing done in RGB layout, diagonal pattern, lowest pixel first and number of characters equal to image height
-        img = Image.open(path)
-        pixels = img.load()
-        message = ""
+    def readHidden(window, path, radio_text, pattern_text, order_text, length):
+        if path:
+            try:
+                img = Image.open(path)
+                pixels = img.load()
+                message = ""
 
-        x = 0
-        y = 0
+                # Initialize the starting coordinates based on order_text
+                start_x = 0
+                start_y = 0
+                if order_text == 1:
+                    start_x = img.width - 3
+                    start_y = img.height - 3
 
-        while x < img.height:
-            pixel = list(pixels[x, x])
-            message += str(int(pixel[y%3] & 1))
-            x += 1
-            y += 1
+                # Initialize the iteration step based on pattern_text
+                step_x = 0
+                step_y = 0
+                if pattern_text == 0:
+                    step_x = 3
+                elif pattern_text == 1:
+                    step_y = 3
+                elif pattern_text == 2:
+                    step_x = 3
+                    step_y = 3
 
-        output = ''.join(chr(int(message[i:i+8], 2)) for i in range(0, len(message), 8))
-        window.ui.textEdit_text.setText(output)
+                # Determine whether step will be used
+                is_x = 0
+                if step_x == 3:
+                    is_x = 1
+                
+                is_y = 0
+                if step_y == 3:
+                    is_y = 1
+
+                # Iterate through the 2D array based on pattern_text and order_text
+                for i in range(length):
+                    x = start_x + i * step_x
+                    y = start_y + i * step_y
+                    
+                    # Adjust x and y based on order_text
+                    if order_text == 1:
+                        x -= i * step_x
+                        y -= i * step_y
+
+                    if radio_text == "RGB":
+                        message += str(format(pixels[x, y][0] & 1, 'b'))
+                        message += str(format(pixels[x+(1*is_x), y+(1*is_y)][1] & 1, 'b'))
+                        message += str(format(pixels[x+(2*is_x), y+(2*is_y)][2] & 1, 'b'))
+
+                    elif radio_text == "RBG":
+                        message += str(format(pixels[x, y][0] & 1, 'b'))
+                        message += str(format(pixels[x+(1*is_x), y+(1*is_y)][2] & 1, 'b'))
+                        message += str(format(pixels[x+(2*is_x), y+(2*is_y)][1] & 1, 'b'))
+
+                    elif radio_text == "GRB":
+                        message += str(format(pixels[x, y][1] & 1, 'b'))
+                        message += str(format(pixels[x+(1*is_x), y+(1*is_y)][0] & 1, 'b'))
+                        message += str(format(pixels[x+(2*is_x), y+(2*is_y)][2] & 1, 'b'))
+
+                    elif radio_text == "GBR":
+                        message += str(format(pixels[x, y][1] & 1, 'b'))
+                        message += str(format(pixels[x+(1*is_x), y+(1*is_y)][2] & 1, 'b'))
+                        message += str(format(pixels[x+(2*is_x), y+(2*is_y)][0] & 1, 'b'))
+
+                    elif radio_text == "BRG":
+                        message += str(format(pixels[x, y][2] & 1, 'b'))
+                        message += str(format(pixels[x+(1*is_x), y+(1*is_y)][0] & 1, 'b'))
+                        message += str(format(pixels[x+(2*is_x), y+(2*is_y)][1] & 1, 'b'))
+
+                    elif radio_text == "BGR":
+                        message += str(format(pixels[x, y][2] & 1, 'b'))
+                        message += str(format(pixels[x+(1*is_x), y+(1*is_y)][1] & 1, 'b'))
+                        message += str(format(pixels[x+(2*is_x), y+(2*is_y)][0] & 1, 'b'))
+
+                output = ''.join(chr(int(message[i:i+8], 2)) for i in range(0, len(message), 8))
+                logic.writeOutput(window, output)
+            except IsADirectoryError:
+                logic.writeOutput(window, f"'{path}' is a directory, expecting image file.")
+            except FileNotFoundError:
+                logic.writeOutput(window, f"File '{path}' was not found on this computer.")
+            except:
+                logic.writeOutput(window, "Something went wrong.")
+
+    def writeHidden(window, path, radio_text, pattern_text, order_text, length, text):
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QDialog, QApplication
+from PyQt6.QtWidgets import QDialog, QApplication, QRadioButton
 from steganoCracker import Ui_steganoCracker
 from logic import logic
 
@@ -16,15 +16,36 @@ class Window(QDialog):
         super().__init__(parent=None)
         self.ui = Ui_steganoCracker() # Create an instance of the UI class
         self.ui.setupUi(self)
+
+        self.selected_operation = 0 # Class variable to keep track of selected operation
+        self.selected_radio_button = None # Class variable to keep track of selected radio button
+        self.selected_pattern = 0 # Class variable to keep track of selected pattern
+        self.selected_order = 0 # Class variable to keep track of selected order
+        self.ui.label_number.setVisible(0)
+        self.ui.spinBox_number.setVisible(0)
+
         self._connectSignals()
 
     def _connectSignals(self):
         self.ui.pushButton_quit.clicked.connect(lambda: QApplication.quit()) # Connect the signal to the function
-        self.ui.pushButton_browse.clicked.connect(self.openFile) # Connect the signal to the function
+        self.ui.pushButton_browse.clicked.connect(self.openFile)
         self.ui.lineEdit_path.returnPressed.connect(lambda: self.displayImage(self.ui.lineEdit_path.text()))
         self.ui.lineEdit_path.textChanged.connect(lambda: self.displayImage(self.ui.lineEdit_path.text()))
-        self.ui.comboBox_operation.currentIndexChanged.connect(lambda: self.readOnly(self.ui.comboBox_operation.currentIndex()))
-        self.ui.pushButton_execute.clicked.connect(lambda: self.readHidden(self.ui.lineEdit_path.text()))
+        self.ui.comboBox_operation.currentIndexChanged.connect(lambda: self.readOperation(self.ui.comboBox_operation.currentIndex()))
+
+        # Connect radio buttons to a function that handles their clicks
+        self.ui.radioButton_rgb.animateClick() # Makes sure to emit clicked signal on RGB
+        self.ui.radioButton_rgb.clicked.connect(self.radio_button_clicked)
+        self.ui.radioButton_rbg.clicked.connect(self.radio_button_clicked)
+        self.ui.radioButton_grb.clicked.connect(self.radio_button_clicked)
+        self.ui.radioButton_gbr.clicked.connect(self.radio_button_clicked)
+        self.ui.radioButton_brg.clicked.connect(self.radio_button_clicked)
+        self.ui.radioButton_bgr.clicked.connect(self.radio_button_clicked)
+
+        self.ui.comboBox_pattern.currentIndexChanged.connect(lambda: self.pattern_changed(self.ui.comboBox_pattern.currentIndex()))
+        self.ui.comboBox_order.currentIndexChanged.connect(lambda: self.order_changed(self.ui.comboBox_order.currentIndex()))
+
+        self.ui.pushButton_execute.clicked.connect(lambda: self.executeButton(self.ui.lineEdit_path.text()))
 
     def writeOutput(self, text):
         logic.writeOutput(self, text)
@@ -35,11 +56,36 @@ class Window(QDialog):
     def displayImage(self, path):
         logic.displayImage(self, path)
 
-    def readOnly(self, option):
-        logic.readOnly(self, option)
+    def readOperation(self, option):
+        self.selected_operation = option
+        logic.readOperation(self, option)
 
     def readHidden(self, path):
         logic.readHidden(self, path)
+
+    def radio_button_clicked(self):
+        sender = self.sender() # Get the radio button that triggered the signal
+        if isinstance(sender, QRadioButton):
+            self.selected_radio_button = sender # Update the selected radio button
+
+    def pattern_changed(self, option):
+        self.selected_pattern = option
+
+    def order_changed(self, option):
+        self.selected_order = option
+
+    def executeButton(self, path):
+        radio_text = self.selected_radio_button.text() # Get the text of the selected radio button
+        pattern_text = self.selected_pattern
+        order_text = self.selected_order
+
+        if self.selected_operation == 0:
+            text = self.ui.textEdit_text.toPlainText() # Get the text from the textEdit_text
+            length = len(text) # Calculate the length of the text
+            logic.writeHidden(self, path, radio_text, pattern_text, order_text, length, text)
+        else:
+            length = (self.ui.spinBox_number.value() - 3) * 3
+            logic.readHidden(self, path, radio_text, pattern_text, order_text, length)
 
 if __name__ == "__main__":
     app = QApplication([])
