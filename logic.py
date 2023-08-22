d9e916066b812c29af75d0a6605c3c0173acaa99
tTargiel <13293432+tTargiel@users.noreply.github.com>
2023-08-22 03:47:59 +0200

    Implemented writeHidden function, added option to specify subpixels LSb modification

--- logic.py
+++ logic.py
@@ -52,7 +52,7 @@ class logic():
     def showNumber(window, option):
         window.ui.textEdit_text.setReadOnly(option)
 
-    def readHidden(window, path, radio_text, pattern_text, order_text, length):
+    def readHidden(window, path, radio_text, pattern_text, order_text, length, subpixels_text, variable):
         if path:
             try:
                 img = Image.open(path)
@@ -63,27 +63,27 @@ class logic():
                 start_x = 0
                 start_y = 0
                 if order_text == 1:
-                    start_x = img.width - 3
-                    start_y = img.height - 3
+                    start_x = img.width - variable
+                    start_y = img.height - variable
 
                 # Initialize the iteration step based on pattern_text
                 step_x = 0
                 step_y = 0
                 if pattern_text == 0:
-                    step_x = 3
+                    step_x = variable
                 elif pattern_text == 1:
-                    step_y = 3
+                    step_y = variable
                 elif pattern_text == 2:
-                    step_x = 3
-                    step_y = 3
+                    step_x = variable
+                    step_y = variable
 
                 # Determine whether step will be used
                 is_x = 0
-                if step_x == 3:
+                if (step_x == variable) and (subpixels_text == 1):
                     is_x = 1
                 
                 is_y = 0
-                if step_y == 3:
+                if (step_y == variable) and (subpixels_text == 1):
                     is_y = 1
 
                 # Iterate through the 2D array based on pattern_text and order_text
@@ -135,5 +135,142 @@ class logic():
             except:
                 logic.writeOutput(window, "Something went wrong.")
 
-    def writeHidden(window, path, radio_text, pattern_text, order_text, length, text):
-        pass
+    def writeHidden(window, path, radio_text, pattern_text, order_text, length, text, subpixels_text, variable):
+        if path:
+            try:
+                img = Image.open(path)
+                binary_msg = ''.join(format(ord(char), '08b') for char in text)
+                for i in range(len(binary_msg) % 3):
+                    binary_msg += format(ord(" "), '08b')
+                pixels = img.load()
+                msg_index = 0
+
+                # Initialize the starting coordinates based on order_text
+                start_x = 0
+                start_y = 0
+                if order_text == 1:
+                    start_x = img.width - variable
+                    start_y = img.height - variable
+
+                # Initialize the iteration step based on pattern_text
+                step_x = 0
+                step_y = 0
+                if pattern_text == 0:
+                    step_x = variable
+                elif pattern_text == 1:
+                    step_y = variable
+                elif pattern_text == 2:
+                    step_x = variable
+                    step_y = variable
+
+                # Determine whether step will be used
+                is_x = 0
+                if (step_x == variable) and (subpixels_text == 1):
+                    is_x = 1
+                
+                is_y = 0
+                if (step_y == variable) and (subpixels_text == 1):
+                    is_y = 1
+
+                # Iterate through the 2D array based on pattern_text and order_text
+                for i in range(len(binary_msg)):
+                    x = start_x + i * step_x
+                    y = start_y + i * step_y
+                    
+                    # Adjust x and y based on order_text
+                    if order_text == 1:
+                        x -= i * step_x
+                        y -= i * step_y
+
+                    if radio_text == "RGB":
+                        r, g, b = pixels[x, y]
+                        r = (r & ~1) | int(binary_msg[msg_index])
+                        pixels[x, y] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (1 * is_x), y + (1 * is_y)]
+                        g = (g & ~1) | int(binary_msg[msg_index + 1])
+                        pixels[x + (1 * is_x), y + (1 * is_y)] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (2 * is_x), y + (2 * is_y)]
+                        b = (b & ~1) | int(binary_msg[msg_index + 2])
+                        pixels[x + (2 * is_x), y + (2 * is_y)] = (r, g, b)
+
+                    elif radio_text == "RBG":
+                        r, g, b = pixels[x, y]
+                        r = (r & ~1) | int(binary_msg[msg_index])
+                        pixels[x, y] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (1 * is_x), y + (1 * is_y)]
+                        b = (b & ~1) | int(binary_msg[msg_index + 1])
+                        pixels[x + (1 * is_x), y + (1 * is_y)] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (2 * is_x), y + (2 * is_y)]
+                        g = (g & ~1) | int(binary_msg[msg_index + 2])
+                        pixels[x + (2 * is_x), y + (2 * is_y)] = (r, g, b)
+
+                    elif radio_text == "GRB":
+                        r, g, b = pixels[x, y]
+                        g = (g & ~1) | int(binary_msg[msg_index])
+                        pixels[x, y] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (1 * is_x), y + (1 * is_y)]
+                        r = (r & ~1) | int(binary_msg[msg_index + 1])
+                        pixels[x + (1 * is_x), y + (1 * is_y)] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (2 * is_x), y + (2 * is_y)]
+                        b = (b & ~1) | int(binary_msg[msg_index + 2])
+                        pixels[x + (2 * is_x), y + (2 * is_y)] = (r, g, b)
+
+                    elif radio_text == "GBR":
+                        r, g, b = pixels[x, y]
+                        g = (g & ~1) | int(binary_msg[msg_index])
+                        pixels[x, y] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (1 * is_x), y + (1 * is_y)]
+                        b = (b & ~1) | int(binary_msg[msg_index + 1])
+                        pixels[x + (1 * is_x), y + (1 * is_y)] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (2 * is_x), y + (2 * is_y)]
+                        r = (r & ~1) | int(binary_msg[msg_index + 2])
+                        pixels[x + (2 * is_x), y + (2 * is_y)] = (r, g, b)
+
+                    elif radio_text == "BRG":
+                        r, g, b = pixels[x, y]
+                        b = (b & ~1) | int(binary_msg[msg_index])
+                        pixels[x, y] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (1 * is_x), y + (1 * is_y)]
+                        r = (r & ~1) | int(binary_msg[msg_index + 1])
+                        pixels[x + (1 * is_x), y + (1 * is_y)] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (2 * is_x), y + (2 * is_y)]
+                        g = (g & ~1) | int(binary_msg[msg_index + 2])
+                        pixels[x + (2 * is_x), y + (2 * is_y)] = (r, g, b)
+
+                    elif radio_text == "BGR":
+                        r, g, b = pixels[x, y]
+                        b = (b & ~1) | int(binary_msg[msg_index])
+                        pixels[x, y] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (1 * is_x), y + (1 * is_y)]
+                        g = (g & ~1) | int(binary_msg[msg_index + 1])
+                        pixels[x + (1 * is_x), y + (1 * is_y)] = (r, g, b)
+                        
+                        r, g, b = pixels[x + (2 * is_x), y + (2 * is_y)]
+                        r = (r & ~1) | int(binary_msg[msg_index + 2])
+                        pixels[x + (2 * is_x), y + (2 * is_y)] = (r, g, b)
+
+                    msg_index += 3
+                    if msg_index >= len(binary_msg):
+                        break
+
+                filename, extension = path.split('.')
+                path = f"{filename}_output.{extension}"
+                img.save(path)
+                logic.writeOutput(window, f"Saved to '{path}'")
+            except IsADirectoryError:
+                logic.writeOutput(window, f"'{path}' is a directory, expecting image file.")
+            except FileNotFoundError:
+                logic.writeOutput(window, f"File '{path}' was not found on this computer.")
+            # except:
+                # logic.writeOutput(window, "Something went wrong.")
--- main.py
+++ main.py
@@ -20,6 +20,8 @@ class Window(QDialog):
         self.selected_operation = 0 # Class variable to keep track of selected operation
         self.selected_radio_button = None # Class variable to keep track of selected radio button
         self.selected_pattern = 0 # Class variable to keep track of selected pattern
+        self.selected_subpixels = 0 # Class variable to keep track of selected subpixels
+        self.variable = 1 # Change point of length variance calculation
         self.selected_order = 0 # Class variable to keep track of selected order
         self.ui.label_number.setVisible(0)
         self.ui.spinBox_number.setVisible(0)
@@ -43,6 +45,7 @@ class Window(QDialog):
         self.ui.radioButton_bgr.clicked.connect(self.radio_button_clicked)
 
         self.ui.comboBox_pattern.currentIndexChanged.connect(lambda: self.pattern_changed(self.ui.comboBox_pattern.currentIndex()))
+        self.ui.comboBox_subpixels.currentIndexChanged.connect(lambda: self.subpixels_changed(self.ui.comboBox_subpixels.currentIndex()))
         self.ui.comboBox_order.currentIndexChanged.connect(lambda: self.order_changed(self.ui.comboBox_order.currentIndex()))
 
         self.ui.pushButton_execute.clicked.connect(lambda: self.executeButton(self.ui.lineEdit_path.text()))
@@ -71,6 +74,10 @@ class Window(QDialog):
     def pattern_changed(self, option):
         self.selected_pattern = option
 
+    def subpixels_changed(self, option):
+        self.variable = 1 if option == 0 else 3
+        self.selected_subpixels = option
+
     def order_changed(self, option):
         self.selected_order = option
 
@@ -78,14 +85,16 @@ class Window(QDialog):
         radio_text = self.selected_radio_button.text() # Get the text of the selected radio button
         pattern_text = self.selected_pattern
         order_text = self.selected_order
+        subpixels_text = self.selected_subpixels
+        variable = self.variable
 
         if self.selected_operation == 0:
             text = self.ui.textEdit_text.toPlainText() # Get the text from the textEdit_text
             length = len(text) # Calculate the length of the text
-            logic.writeHidden(self, path, radio_text, pattern_text, order_text, length, text)
+            logic.writeHidden(self, path, radio_text, pattern_text, order_text, length, text, subpixels_text, variable)
         else:
-            length = (self.ui.spinBox_number.value() - 3) * 3
-            logic.readHidden(self, path, radio_text, pattern_text, order_text, length)
+            length = self.ui.spinBox_number.value() * 3 - variable
+            logic.readHidden(self, path, radio_text, pattern_text, order_text, length, subpixels_text, variable)
 
 if __name__ == "__main__":
     app = QApplication([])
